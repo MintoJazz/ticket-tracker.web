8f9fcc5b7eed5507db7597686b665a463c97ed9a
MintoJazz <jinzo.matt@gmail.com>
2026-09-09 17:57:22 -0300

    chore: configurando routrer no main

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,35 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { createBrowserRouter } from "react-router"
+import { RouterProvider } from "react-router/dom"
 
 import "./index.css"
-import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { routes } from "./config/routes"
 
 async function enableMocking() {
   if (!import.meta.env.DEV) {
     return
   }
- 
-  const { worker } = await import('./mocks/browser')
+
+  const { worker } = await import("./mocks/browser")
   return worker.start()
 }
 
-enableMocking().then(() => {
-  createRoot(document.getElementById("root")!).render(
+function createRouter() {
+  return createBrowserRouter(routes)
+}
+
+function renderRoot() {
+  const router = createRouter()
+
+  return createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <ThemeProvider>
-        <App />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </StrictMode>
   )
-})
+}
+
+enableMocking().then(renderRoot)
